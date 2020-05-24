8ed0dc02c2955a206e03936ef0b0dee2ac3cd59f
aokiyasuhiro8376 <56961562+aokiyasuhiro8376@users.noreply.github.com>
2020-05-24 21:54:21 +0900

    Update rooms_controller.rb

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RoomsController < ApplicationController
-  # before_action :authenticate_user!
+  before_action :authenticate_user!
   # before_action :set_room, except: [:create]
   before_action :move_to_index, except: [:index]
 
